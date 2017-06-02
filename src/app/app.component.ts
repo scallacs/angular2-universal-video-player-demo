5e57a0cb9d5f2245c752d2d1b7f98034e10754b4
Stephane <sca.leonard@gmail.com>
2017-06-02 15:16:17 +0200

    first add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { SampleModule } from 'angular2-universal-player-api';
+import { Router, ActivatedRoute, Params} from '@angular/router'; 
+
+import { UniversalPlayerController, YoutubeCmdMapper, VideoInfo} from 'angular2-universal-player-api';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public title = 'app works!';
+
+  public playerController = new UniversalPlayerController();
+  public playerEvent = new UniversalPlayerController();
+  public playerCmd = new YoutubeCmdMapper();
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    console.log('Route params: ', this.route.params);
+    this.route.params.forEach((params: Params) => {
+      let id = +params['slug']; // (+) converts string 'id' to a number
+      // TODO load video
+      if (id != null) {
+        this.loadVideo(id);
+      }
+      else {
+        console.error('Invalid params: ' + id);
+        // TODO redirect to not found ?
+      }
+    });
+  }
+
+  private loadVideo(id: number) {
+    // TODO 
+    console.log('Loading video with id:' + id);
+
+    // this.videosTable.view(id)
+    //   .subscribe(
+    //   video => {
+    //     //                        this.video = video;
+    //     //let videoInfo = new VideoInfo();
+    //     this.playerController.loadVideo(video);
+    //     console.log(video);
+    //     //                        this.playerCmd.load();
+    //   }
+    //   );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SampleModule } from 'angular2-universal-player-api';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
